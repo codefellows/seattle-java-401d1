728d9aa153649bee68fc40297b2741e82d8ba7a9
Steve Geluso <stevegeluso@gmail.com>
2018-07-30 11:15:38 -0700

    deleting dupe

--- class-15-serving-uploaded-files-with-auth/SongUploads2/src/main/java/server/MockDB.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MockDB {
-    public static final List<Song> songs = new ArrayList<>();
-}
--- class-15-serving-uploaded-files-with-auth/SongUploads2/src/main/java/server/Song.java
+++
@@ -1,7 +0,0 @@
-package server;
-
-public class Song {
-    public String artist;
-    public String title;
-    public String path;
-}
