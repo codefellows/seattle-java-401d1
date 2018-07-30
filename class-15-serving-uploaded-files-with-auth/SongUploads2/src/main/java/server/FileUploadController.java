603234b8ea089e60b0958053d2ed4bb889a77908
Steve Geluso <stevegeluso@gmail.com>
2018-07-30 11:13:08 -0700

    fixing songuploads dem

---
+++ class-15-serving-uploaded-files-with-auth/SongUploads2/src/main/java/server/FileUploadController.java
@@ -0,0 +1,74 @@
+package server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import server.storage.FileSystemStorageService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+@Controller
+public class FileUploadController {
+
+    private final FileSystemStorageService storageService;
+
+    @Autowired
+    public FileUploadController(FileSystemStorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/")
+    public String listUploadedFiles(Model model) throws IOException {
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+
+        return "uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        try {
+            Resource file = storageService.loadAsResource(filename);
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        } catch (IOException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(
+            @RequestParam("artist") String artist,
+            @RequestParam("song") String song,
+            @RequestParam("file") MultipartFile file,
+            Model model
+    ) {
+        try {
+            String filepath = storageService.store(file);
+            filepath = filepath.split("static")[1];
+            System.out.println(filepath);
+
+            Song mp3 = new Song();
+            mp3.artist = artist;
+            mp3.title = song;
+            mp3.path = filepath;
+
+            MockDB.songs.add(mp3);
+        } catch (IOException e) {
+
+        }
+        return "redirect:/songs";
+    }
+}
