32ecdb8fc00f8acaf839768474c561b5af35370f
Steve Geluso <stevegeluso@gmail.com>
2018-08-01 15:40:22 -0700

    able to render data from controller in template

--- class-18-spring-security-against-user-input/demos/CRUDTrek/src/main/java/com/example/demo/controllers/ControllerHome.java
+++ class-18-spring-security-against-user-input/demos/CRUDTrek/src/main/java/com/example/demo/controllers/ControllerHome.java
@@ -1,15 +1,22 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+
 @Controller
 public class ControllerHome {
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
         System.out.println("hit home controller");
+
+        Date date = new Date();
+        model.addAttribute("currenttime", date.toString());
+
         return "index";
     }
 }
