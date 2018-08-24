ee76c64d3ea3ae9dcb5bd8d4633c94e401fa111c
Steve Geluso <stevegeluso@gmail.com>
2018-08-23 18:14:03 -0700

    adding status class

--- class-33-firebase-realtime-database/demos/ClickingFirebaseRealtimeDatabase/app/src/main/java/com/example/moonmayor/clickingfirebaserealtimedatabase/Status.java
+++ class-33-firebase-realtime-database/demos/ClickingFirebaseRealtimeDatabase/app/src/main/java/com/example/moonmayor/clickingfirebaserealtimedatabase/Status.java
@@ -1,11 +1,15 @@
 package com.example.moonmayor.clickingfirebaserealtimedatabase;
 
 public class Status {
-    public String username;
-    public String status;      // one of "online", "away", "offline"
-    public String statusText;  // "I'm grabbing a sandwhich"
+    public enum AwayStatus {
+        ONLINE, AWAY, OFFLINE
+    }
 
-    public Status(String username, String status, String statusText) {
+    public String username;
+    public AwayStatus status;      // one of "online", "away", "offline"
+    public String statusText;      // "I'm grabbing a sandwhich"
+
+    public Status(String username, AwayStatus status, String statusText) {
         this.username = username;
         this.status = status;
         this.statusText = statusText;
